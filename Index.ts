c07720088703ca1ddd6dff6d02f341fa5fac2d35
harut <hharutyunyan111@gmail.com>
2019-06-15 20:48:13 +0400

    initial commitqq

--- Index.ts
+++ Index.ts
@@ -8,6 +8,26 @@ if (!cluster.isMaster) {
     const port: number | string = process.env.PORT || 3000;
     new Server().Start().then((server) => {
         server.listen(port);
+        server.on("error", (error: any) => {
+            if (error.syscall !== "listen") {
+                throw error;
+            }
+            switch (error.code) {
+                case "EACCES":
+                    console.error("Port requires elevated privileges");
+                    process.exit(1);
+                    break;
+                case "EADDRINUSE":
+                    console.error("Port is already in use");
+                    process.exit(1);
+                    break;
+                default:
+                    throw error;
+            }
+        });
+        server.on("listening", () => {
+            console.log("Server is running in process " + process.pid + " listening on PORT " + port + "\n");
+        });
     });
 } else {
     console.log(`\n -------------------> RUN ${env.NODE_ENV} ENVIRONMENT \n`);
