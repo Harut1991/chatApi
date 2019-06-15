e0ae751d3dd976913dfa141c39a4ebb9ec32c6e6
harut <hharutyunyan111@gmail.com>
2019-06-15 18:59:42 +0400

    change Database for herouko

--- config.ts
+++ config.ts
@@ -1,28 +1,11 @@
 import { env } from "process";
 
-export const DIALECT = "mysql";
-
-const LOCAL_CONFIGURATION = {
-    DB: "node",
-    PASSWORD: "root",
-    PORT_DB: 3306,
-    SERVER: "127.0.0.1",
-    USER_DB: "regroup",
-};
-
-const PRODUCTION_CONFIGURATION = {
-    DB: env.DB,
-    PASSWORD: env.PASSWORD,
-    PORT_DB: Number(env.PORT_DB),
-    SERVER: env.SERVER,
-    USER_DB: env.USER_DB,
-};
+export const DIALECT = "sqlite";
 
 export function isProduction(): boolean {
     return env.NODE_ENV === "PRODUCTION";
 }
 
 export const config = {
-    DATABASE: isProduction() ? PRODUCTION_CONFIGURATION : LOCAL_CONFIGURATION,
     PORT_APP: 8080,
 };
--- config/Database.ts
+++ config/Database.ts
@@ -1,20 +1,15 @@
 import { createConnection } from "typeorm";
 import {Message, Room, User} from "../app/models";
-import { config, DIALECT } from "../config";
+import {DIALECT } from "../config";
 
 export const Connection = createConnection({
-    database: config.DATABASE.DB,
-    charset: 'utf8mb4',
-    entities: [
-        User,
-        Message,
-        Room,
-    ],
-    host: config.DATABASE.SERVER,
-    logging: false,
-    password: config.DATABASE.PASSWORD,
-    port: config.DATABASE.PORT_DB,
-    synchronize: true,
     type: DIALECT,
-    username: config.DATABASE.USER_DB,
+    database: `../chat.db`,
+    entities: [
+            User,
+            Message,
+            Room,
+    ],
+    logging: true,
+    synchronize: true,
 });
