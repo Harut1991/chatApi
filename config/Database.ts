917d17e64e882ea9e7c573659b5895f6ed5650fc
harut <hharutyunyan111@gmail.com>
2019-06-16 11:26:30 +0400

    inits

--- config/Database.ts
+++ config/Database.ts
@@ -1,9 +1,11 @@
+import { env } from "process";
 import { createConnection } from "typeorm";
 import {Message, Room, User} from "../app/models";
 import {DIALECT} from "../config";
 const path = require("path");
-const dbPath = path.resolve("chat.db");
+const dbPath = path.resolve( env.NODE_ENV === "development" ? "chat.db" : "../chat.db");
 
+console.log(dbPath);
 export const Connection = createConnection({
     type: DIALECT,
     database: dbPath,
